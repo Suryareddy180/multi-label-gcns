14623a896e1aa01939a7a16024be75fb267ac24b
Surya Prakash <suryareddynallimilli@gmail.com>
2025-06-04 14:30:08 +0530

    commit

--- src/model/backbone/resnet.py
+++ src/model/backbone/resnet.py
@@ -2,27 +2,24 @@ import torch.nn as nn
 from torchvision import models
 
 
-import torch.nn as nn
-from torchvision import models
-
-
 class ResNet(nn.Module):
     def __init__(
         self,
         num_layers: int = 50,
-        pretrained: bool = False,  # Explicit control over pretraining
+        weights: str = None,
         progress: bool = True
     ):
         super(ResNet, self).__init__()
         assert num_layers in [34, 50, 101]
+        assert weights in ["IMAGENET1K_V1", "IMAGENET1K_V2"]
 
-        # Define the model based on num_layers and pretrained setting
+        params = {"weights": weights, "progress": progress}
         if num_layers == 34:
-            self.resnet = models.resnet34(weights=None if not pretrained else models.ResNet34_Weights.DEFAULT, progress=progress)
+            self.resnet = models.resnet34(**params)
         elif num_layers == 50:
-            self.resnet = models.resnet50(weights=None if not pretrained else models.ResNet50_Weights.DEFAULT, progress=progress)
+            self.resnet = models.resnet50(**params)
         else:
-            self.resnet = models.resnet101(weights=None if not pretrained else models.ResNet101_Weights.DEFAULT, progress=progress)
+            self.resnet = models.resnet101(**params)
 
     def get_backbone(self):
         return nn.Sequential(*list(self.resnet.children())[:-2])
