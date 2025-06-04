9723114a4b5cf5669da0e8eda126aa02a1b1e3a0
Surya Prakash <suryareddynallimilli@gmail.com>
2025-06-04 14:35:32 +0530

    commi

--- src/model/backbone/resnet.py
+++ src/model/backbone/resnet.py
@@ -1,22 +1,27 @@
 import torch.nn as nn
 from torchvision import models
 
+
 class ResNet(nn.Module):
-    def _init_(self, num_layers: int = 50, weights: str = None, progress: bool = True):
-        super(ResNet, self)._init_()
+    def __init__(
+        self,
+        num_layers: int = 50,
+        weights: str = "IMAGENET1K_V2",
+        progress: bool = True
+    ):
+        super(ResNet, self).__init__()
+        assert num_layers in [34, 50, 101]
+        assert weights in ["IMAGENET1K_V1", "IMAGENET1K_V2"]
 
-        assert num_layers in [34, 50, 101], "Only ResNet-34, 50, and 101 are supported."
-
-        # Select appropriate model without pretrained weights (weights=None)
+        params = {"weights": weights, "progress": progress}
         if num_layers == 34:
-            self.resnet = models.resnet34(weights=None, progress=progress)
+            self.resnet = models.resnet34(**params)
         elif num_layers == 50:
-            self.resnet = models.resnet50(weights=None, progress=progress)
+            self.resnet = models.resnet50(**params)
         else:
-            self.resnet = models.resnet101(weights=None, progress=progress)
+            self.resnet = models.resnet101(**params)
 
     def get_backbone(self):
-        # Removes last two layers (avgpool and fc)
         return nn.Sequential(*list(self.resnet.children())[:-2])
 
     def forward(self, x):
